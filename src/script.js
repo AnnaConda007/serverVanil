4a69b3d9eb115afc498fb4c79f01b15cd4a66a09
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-01 20:54:44 +0400

    добавлена рекурсия при отрисовке

--- src/script.js
+++ src/script.js
@@ -9,19 +9,25 @@ const createList = async () => {
     const data = await users();
     for (let i = 0; i < data.length; i++) {
         const ul = document.createElement("ul");
-        document.querySelector(".list-wrap").appendChild(ul)
-        ul.innerHTML += `<li>${data[i].name}</li>
-        <li>${data[i].username}</li>
-        <li>${data[i].email}</li>
-        <li>${data[i].phone}</li>
-`
-
+        document.querySelector(".list-wrap").appendChild(ul);
+        for (let prop in data[i]) {
+            const li = document.createElement("li");
+            if (typeof data[i][prop] === 'object' && data[i][prop] !== null) {
+                li.textContent = `${prop}:`;
+                const innerUl = document.createElement("ul");
+                li.appendChild(innerUl);
+                for (let innerProp in data[i][prop]) {
+                    const nestedLi = document.createElement("li");
+                    nestedLi.textContent = `${innerProp}: ${data[i][prop][innerProp]}`;
+                    innerUl.appendChild(nestedLi);
+                }
+            } else {
+                li.textContent = `${prop}: ${data[i][prop]}`;
+            }
+            ul.appendChild(li);
+        }
     }
-
-
-
-}
-
+};
 
 document.querySelector("button").addEventListener("click", () => { createList() })
 
