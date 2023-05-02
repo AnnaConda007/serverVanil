d2fbddda7de3ab68e6c6522831ea23842c71ca5b
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-02 11:41:12 +0400

    build

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const cssnano = require('gulp-cssnano');
+const htmlmin = require('gulp-htmlmin');
+const strip = require('gulp-strip-comments');
+const replace = require('gulp-replace');
 const rename = require('gulp-rename');
+
+const copy = require('gulp-copy');
 
 gulp.task('dev', function () {
     gulp.watch(['src/js/**/*.js', 'src/css/**/*.css'], gulp.series('build'));
@@ -10,12 +15,20 @@ gulp.task('dev', function () {
 
 gulp.task('build', function (done) {
     gulp.src('src/js/**/*.js')
-        .pipe(concat('app.min.js'))
+        .pipe(concat('script.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
-    gulp.src('src/css/**/*.css')
-        .pipe(concat('app.min.css'))
+    gulp.src('src/css/bootstrap.min.css')
+        .pipe(copy('dist/css', { prefix: 2 }))
+    gulp.src(['src/css/**/*.css', '!src/css/bootstrap.min.css'])
+        .pipe(concat('style.min.css'))
         .pipe(cssnano())
         .pipe(gulp.dest('dist/css'))
+    gulp.src('src/*.html')
+        .pipe(strip())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(replace('style.css', 'style.min.css'))
+        .pipe(replace('script.js', 'script.min.js'))
+        .pipe(gulp.dest('dist'))
         .on('end', done);
 })
