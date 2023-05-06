554c0fc040ede90ee95cd97b91726b9915527cf2
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-06 13:08:03 +0400

    автозаполнение работает только 30 сек

--- src/js/modules/authorization.js
+++ src/js/modules/authorization.js
@@ -5,6 +5,8 @@ const authorization = async () => {
 	const btn = document.querySelector('.btn-authorization');
 	const loginInput = document.querySelector('#login');
 	const passwordInput = document.querySelector('#password');
+	const currentTime = Math.floor(Date.now() / 1000);
+	let loginFromStorage = false;
 
 	const matchAuthorization = () => {
 		let match;
@@ -16,6 +18,10 @@ const authorization = async () => {
 			if (match) {
 				window.location.href = 'success.html';
 				error.classList.remove('visible-element');
+				localStorage.setItem('login', login);
+				localStorage.setItem('password', password);
+
+				localStorage.setItem('isExpired', currentTime + 30);
 				break;
 			} else {
 				error.classList.add('visible-element');
@@ -29,5 +35,18 @@ const authorization = async () => {
 			matchAuthorization();
 		}
 	});
+
+	const autocomplete = () => {
+		const isExpired = localStorage.getItem('isExpired');
+		if (!loginFromStorage && isExpired >= currentTime) {
+			const usedLogin = localStorage.getItem('login');
+			const usedPassword = localStorage.getItem('password');
+			loginInput.value = usedLogin;
+			passwordInput.value = usedPassword;
+			loginFromStorage = true;
+		}
+	};
+	loginInput.addEventListener('click', autocomplete);
 };
+
 export default authorization;
