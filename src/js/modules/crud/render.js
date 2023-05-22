7022fe8e9978bdee8fe33cf353840b9eea71845a
Anna Hrulckova <annahrulkova@yandex.ru>
2023-05-22 22:30:39 +0400

    изменено положение кнопок

--- src/js/modules/crud/render.js
+++ src/js/modules/crud/render.js
@@ -2,17 +2,19 @@ export const render = (taskList, tasks) => {
 	taskList.innerHTML = '';
 	tasks.forEach((task, index) => {
 		taskList.innerHTML += `
-      <li class="list-group-item mt-2 list-item">
-        <div>
-          <span class="list-item__value" data-index="${index}">${task}</span>
-          <button class="bg-transparent border-0 edit-btn" data-index="${index}">
-            <img src="img/pencil.svg" alt="редактировать" />
-          </button>
-          <button class="bg-transparent border-0 delete-btn" data-index="${index}">
-            <img src="img/trash.svg" alt="удалить" />
-          </button>
-        </div>
-      </li>
+   <li class="list-group-item mt-2 list-item">
+	<div class="d-flex justify-content-between ">
+		<span class="list-item__value" data-index="${index}">${task}</span>
+		<div>
+			<button class="bg-transparent border-0 edit-btn" data-index="${index}">
+				<img src="img/pencil.svg" alt="редактировать" />
+			</button>
+			<button class="bg-transparent border-0 delete-btn" data-index="${index}">
+				<img src="img/trash.svg" alt="удалить" />
+			</button>
+		</div>
+	</div>
+</li>
     `;
 	});
 };
